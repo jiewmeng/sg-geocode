ca2fcb1a3aaad87d1bbf8cafaa1464beecd1fbd3
Jiew Meng <jiewmeng@gmail.com>
2016-02-13 15:37:50 +0800

    use google maps sdk

--- public/scripts.js
+++ public/scripts.js
@@ -3,34 +3,23 @@
 document.getElementById('frmSearch').addEventListener('submit', function(e) {
 	e.preventDefault();
 	let postalCodes = document.getElementById('postalCodes').value.trim().split('\n').map(v => v.trim());
-	// let htmlRows = postalCodes.map(code => {
-	// 	let html = '';
-
-	// 	html += `<tr>`;
-	// 	html += `<td>${code}</td>`;
-	// 	html += `<td>?</td>`;
-	// 	html += `<td>?</td>`;
-	// 	html += `</tr>`;
-
-	// 	return html;
-	// }).join('');
 
 	let obj = {};
+	let geocoder = google.maps.Geocoder;
 	Promise.map(postalCodes, (code) => {
 		return new Promise(function(resolve, reject) {
 			if (!code) return resolve({lat: 'NA', lng: 'NA'})
 
-			superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=singapore+${code}&key=AIzaSyAiJKpaQbRJds5N2YGMKWqkikkBCqek2bk`)
-				.end(function(err, res) {
-					if (err) return reject(err);
+			geocoder.geocode({ address: `Singapore ${code}`, function(results, status) {
+				if (status !== google.maps.GeocoderStatus.OK) return reject(status);
 
-					let results = res.body.results;
-					let result = results.find(res => res && res.geometry && res.geometry.location); 
+				console.log(results)
+				let result = results.find(res => res && res.geometry && res.geometry.location); 
 
-					setTimeout(() => {
-						resolve(result ? result.geometry.location : {lat: '?', lng: `"${code}"`});	
-					}, 500);
-				});
+				setTimeout(() => {
+					resolve(result ? result.geometry.location : {lat: '?', lng: `"${code}"`});	
+				}, 500);
+			});
 
 		}).then(coords => obj[code] = coords)
 	}, {concurrency: 5})
